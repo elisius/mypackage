6b4bc25bfb6b26432ad25f8a9de75b993b4e3209
Elisius Legodi <elisiuslegodi@gmail.com>
2021-07-06 15:01:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
-    install_requires=['numpy','tqdm','pip'],
+    install_requires=['numpy'],
     url='https://github.com/<username>/<package-name>',
     author='<Your Name>',
     author_email='<Your Email>'
